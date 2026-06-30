993ee8451a8d6702ad4f2aeb554775cba319bdfb
dingjinhong <dingjinhong123@qq.com>
2026-06-30 09:27:06 +0800

    fix: add comments

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "",
+  //base: "/token-audit/",
+  base:"",
   plugins: [vue()],
   server: {
     port: 5173,
